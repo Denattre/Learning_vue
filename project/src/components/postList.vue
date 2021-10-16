e2e51d901151be2ea4c1b8797886fc78075e8329
Dmitry Ivanov <silen919@gmail.com>
2021-10-16 06:37:14 +0300

    Create postListApp with Vue

---
+++ project/src/components/postList.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h3>Список пользователей</h3>
+  <post-item 
+  v-for="post in posts"
+  :post="post"
+  />
+</div>
+</template>
+
+<script>
+import postItem from './postItem.vue'
+  export default {
+  components: { postItem },
+    props: {
+      posts: {
+        type: Array,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
